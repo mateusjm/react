97243775eb4e1eff2e600e7b9a97204c354ca521
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-07-26 21:34:43 -0300

    avançando nos fundamentos de react

--- 2-fundamentos/fundamentos/src/App.jsx
+++ 2-fundamentos/fundamentos/src/App.jsx
@@ -1,5 +1,8 @@
 // components
 import FirstComponent from './components/FirstComponent'
+import TemplateExpressions from './components/TemplateExpressions'
+import MyComponent from './components/MyComponent'
+import Events from './components/Events'
 
 // styles / CSS
 import { useState } from 'react'
@@ -12,6 +15,9 @@ function App() {
     <>
       <h1>Fundamentos React</h1>
       <FirstComponent />
+      <TemplateExpressions />
+      <MyComponent/>
+      <Events/>
     </>
   )
 }
--- 2-fundamentos/fundamentos/src/components/FirstComponent.jsx
+++ 2-fundamentos/fundamentos/src/components/FirstComponent.jsx
@@ -1,9 +1,16 @@
-// criando primeiro componente
+// arquivos de estilo
+
+import MyComponent from "./MyComponent"
+
 const FirstComponent = () => {
+  // essa função faz isso
+
   return (
     <div>
+      {/* Algum comentário */}
       <h1>Meu primeiro componente</h1>
       <p className="teste">Meu texto</p>
+      <MyComponent/>
     </div>
   )
 }
