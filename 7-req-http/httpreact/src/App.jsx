b942c1ddd0eb95ec211cd3d0868d7f26310ac7d5
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-11-20 18:03:57 -0300

    updates

--- 7-req-http/httpreact/src/App.jsx
+++ 7-req-http/httpreact/src/App.jsx
@@ -1,102 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
-// 4 - custom hook
-import { useFetch } from './hooks/useFetch'
-
-const url = 'http://localhost:3000/products'
-
 function App() {
-
-  const [products, setProducts] = useState([])
-
-  // 4- custom
-  const {data: items, httpConfig, loading} = useFetch(url)
-
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-
-  // 1- resgatando dados
-  /* useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url)
-      const data = await res.json()
-
-      setProducts(data)
-
-    }
-
-    fetchData()
-  }, []) */
-
-  // 2 - adicionando produtos
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    const product = {
-      name,
-      price
-    }
-
-    // configurando um objeto para determinar POST, GET por padrão
-    /* const res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(product),
-    })
-    
-    // 3 - carregamento dinâmico
-    const addedProduct = await res.json()
-
-    setProducts((prevProducts)=> [...prevProducts, addedProduct]) */
-
-    // 5- refatorando post
-    httpConfig(product, 'POST')
-
-    setName('')
-    setPrice('')
-  }
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='App'>
-        <h1>Lista de produtos</h1>
-        {/* 6- loading */}
-        {loading && <p>Carregando dados...</p>}
-        <ul>{items && items.map((product)=> (
-          <li key={product.id}>
-            {product.name} - R$: {product.price}
-          </li>
-        ))}</ul>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <div className='add-product'>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Nome:
-            <input 
-              type="text" 
-              value={name} 
-              name='name' 
-              onChange={(e)=> setName(e.target.value)} 
-            />
-          </label>
-          <label>
-            Preço:
-            <input 
-              type="number" 
-              value={price} 
-              name='price' 
-              onChange={(e)=> setPrice(e.target.value)} 
-            />
-          </label>
-          <input 
-            type="submit" 
-            value="Criar" 
-          />
-        </form>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
