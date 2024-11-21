4f7e5b386e1d621dd99849233b6c610f5ca6186a
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-11-20 21:00:00 -0300

    update

--- 7-req-http/httpreact/src/App.jsx
+++ 7-req-http/httpreact/src/App.jsx
@@ -1,33 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const url = 'http://localhost:3000/products'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [products, setProducts] = useState([])
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+
+  // resgatando os dados
+  useEffect(()=> {
+    const fetchData = async ()=> {
+      const res = await fetch(url)
+      const data = await res.json()
+      setProducts(data)
+    }
+
+    fetchData()
+  }, [])
+
+  // adicionando de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name, 
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "aplication/json"
+      },
+      body: JSON.stringify(product)
+    })
+
+    
+    //carregamento dinamico
+    const addedProduct = await res.json()
+  
+    setProducts((prevProducts)=> [...prevProducts, addedProduct])
+    setName('')
+    setPrice('')
+  }
+
+
+  console.log(products)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <h1>Lista de produtos</h1>
+        <ul>
+          {products.map((product)=> (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}</li>
+          ))}
+        </ul>
+        <div className='add-product'>
+          <form action="" onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input 
+                type="text" 
+                value={name} 
+                name='name' 
+                onChange={(e)=> setName(e.target.value)}/>
+            </label>
+            <label>
+              Preço:
+              <input 
+                type="number" 
+                value={price} 
+                name='price' 
+                onChange={(e)=> setPrice(e.target.value)}/>
+            </label>
+            <input type="submit" value="Criar" />
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
